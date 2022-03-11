495d40f1f7e7922132a3e18a1e9d60a783cfb102
berrypop <48597015+berrypop@users.noreply.github.com>
2022-03-11 11:05:29 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Home from './pages/home/Home';
+import TopBar from './components/topbar/TopBar';
+import Single from './pages/single/Single';
+import Write from './pages/write/Write';
+import Setting from './pages/settings/Setting';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+
+function App() {
+  return (
+    <>
+      <TopBar />
+
+      <Register />
+    </>
+  );
+}
+
+export default App;
+
+// stand 1:27.00 min
